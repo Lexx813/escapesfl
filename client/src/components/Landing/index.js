c42e7709fd937166cfc7295f4860e2a656a4daec
Luis Quinones <alexx813@gmail.com>
2018-09-03 19:58:42 -0500

    adding GA

--- client/src/components/Landing/index.js
+++ client/src/components/Landing/index.js
@@ -1,4 +1,7 @@
 import React from "react";
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-125150549-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 import { Element } from "react-scroll";
 import Featured from "../Featured";
 import About from "../About";
@@ -7,7 +10,7 @@ import Icons from "../homeicons";
 import Gallery from "../Gallery";
 import ContactForm from "../ContactForm";
 import Location from "../Location";
-
+// UA-125150549-1
 const LandingPage = () => {
   return (
     <div>
